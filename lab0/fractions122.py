5da259a80a20f5e86f8deb0731dfae3ad25d35c1
Aso <aaronzhengnz@gmail.com>
2026-07-18 15:33:46 +1200

    Added Fractions122.py file

---
+++ lab0/fractions122.py
@@ -0,0 +1,13 @@
+class Fraction():
+    '''Defines a Fraction type that has an integer numerator and a non-zero integer denominator'''
+
+    def __init__(self, num=0, denom=1):
+        ''' Creates a new Fraction with numerator num and denominator denom'''
+        if isinstance(num, int) and isinstance(denom, int):
+            self.numerator = num
+            if denom != 0:
+                self.denominator = denom
+            else:
+                raise ZeroDivisionError
+        else:
+            raise ValueError('Numerator and denominator must be ints')
